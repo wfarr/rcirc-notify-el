14803cd8cf6c775c806156309ba0928e477b3266
Will Farrington <wcfarrington@gmail.com>
2008-05-22 15:18:42 -0400

    Massive update of function names and adding of accessor functions.

--- rcirc-notify.el
+++ rcirc-notify.el
@@ -22,59 +22,60 @@
 ;; This code is inspired in part by erc-page-me.el and offers
 ;; the same functionality as it, but for rcirc.
 
-(defvar my-rcirc-notify-message "%s is calling your name in %s."
+(defvar rcirc-notification-message "%s is calling your name in %s."
   "Format of message to display in libnotify popup.
 '%s' will expand to the nick that notified you.")
 
-(defvar my-rcirc-notify-nick-alist nil
+(defvar rcirc-notified-nick-alist nil
   "An alist of nicks and the last time they tried to trigger a
 notification.")
 
-(defvar my-rcirc-notify-timeout 30
+(defvar rcirc-notification-timeout 30
   "Number of seconds that will elapse between notifications from the
 same person.")
 
-(defvar my-rcirc-privmsg-target "private message"
+(defvar rcirc-privmsg-target "private message"
   "String used as target in rcirc-notify-message for private messages.")
 
-(defun page-me (title message)
+(defun rcirc-send-notification (title message)
   (cond ((eq window-system 'mac)
-         (start-process "page-me" nil "growlnotify"
+         (start-process "rcirc-notify" nil "growlnotify"
                         "-t" title "-m" message))
         ((eq window-system 'x)
-         (start-process "page-me" nil
+         (start-process "rcirc-notify" nil
                         ;; 8640000 ms = 1 day
                         "notify-send" "-u" "normal" "-i" "gtk-dialog-info"
                         "-t" "8640000" title message))))
 
-(defun my-rcirc-notify (sender &optional target)
-  (unless target (setq target my-rcirc-privmsg-target))
+(defun rcirc-notify (sender &optional target)
+  (unless target (setq target rcirc-privmsg-target))
   (when window-system
     ;; Set default dir to appease the notification gods
     (let ((default-directory "~/"))
-      (page-me "rcirc" (format my-rcirc-notify-message sender target))))
-  (message (concat (format-time-string "%r") " - " (format my-rcirc-notify-message sender target))))
+      (rcirc-send-notification "rcirc" (format rcirc-notification-message sender target))))
+  ;; Always print a copy to *Messages*
+  (message (concat (format-time-string "%r") " - " (format rcirc-notification-message sender target))))
 
-(defun my-rcirc-notify-allowed (sender &optional delay)
+(defun rcirc-notify-allowed (sender &optional delay)
   "Return non-nil if a notification should be made for SENDER.
 If DELAY is specified, it will be the minimum time in seconds
 that can occur between two notifications.  The default is
-`my-rcirc-notify-timeout'."
-  (unless delay (setq delay my-rcirc-notify-timeout))
+`rcirc-notification-timeout'."
+  (unless delay (setq delay rcirc-notification-timeout))
   (let ((cur-time (time-to-seconds (current-time)))
-        (cur-assoc (assoc sender my-rcirc-notify-nick-alist))
+        (cur-assoc (assoc sender rcirc-notified-nick-alist))
         (last-time))
     (if cur-assoc
         (progn
           (setq last-time (cdr cur-assoc))
           (setcdr cur-assoc cur-time)
           (> (abs (- cur-time last-time)) delay))
-      (push (cons sender cur-time) my-rcirc-notify-nick-alist)
+      (push (cons sender cur-time) rcirc-notified-nick-alist)
       t)))
 
-(defun my-rcirc-notify-me (proc sender response target text)
+(defun rcirc-notify-channel (proc sender response target text)
   "Notify the current user when someone sends a message that
-matches a regexp in `rcirc-keywords'."
+matches a regexp in `rcirc-keywords' inside a channel."
   (interactive)
   (when (and (string-match
               (concat "\\(^\\|[^a-zA-Z0-9_\\-]\\)"
@@ -83,20 +84,31 @@ matches a regexp in `rcirc-keywords'."
               text)
              (not (string= (rcirc-nick proc) sender))
              (not (string= (rcirc-server-name proc) sender))
-             (my-rcirc-notify-allowed sender))
-    (my-rcirc-notify sender target)))
+             (rcirc-notify-allowed sender))
+    (rcirc-notify sender target)))
 
-(defun my-rcirc-notify-privmsg (proc sender response target text)
+(defun rcirc-notify-privmsg (proc sender response target text)
   "Notify the current user when someone sends a private message
 to them."
   (interactive)
   (when (and (string= response "PRIVMSG")
              (not (string= sender (rcirc-nick proc)))
              (not (rcirc-channel-p target))
-             (my-rcirc-notify-allowed sender))
-    (my-rcirc-notify sender)))
+             (rcirc-notify-allowed sender))
+    (rcirc-notify sender)))
 
-(add-hook 'rcirc-print-hooks 'my-rcirc-notify-privmsg)
-(add-hook 'rcirc-print-hooks 'my-rcirc-notify-me)
+;;;###autoload
+(defun turn-on-rcirc-notify ()
+  (interactive)
+  (unless (load-history-filename-element "\\rcirc-notify.el")
+    (load "rcirc-notify"))
+  (add-hook 'rcirc-print-hooks 'rcirc-notify-privmsg)
+  (add-hook 'rcirc-print-hooks 'rcirc-notify-channel))
+
+;;;###autoload
+(defun turn-off-rcirc-notify ()
+  (interactive)
+  (remove-hook 'rcirc-print-hooks 'rcirc-notify-channel)
+  (remove-hook 'rcirc-print-hooks 'rcirc-notify-privmsg))
 
 (provide 'rcirc-notify)
