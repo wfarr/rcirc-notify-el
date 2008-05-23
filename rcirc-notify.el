53f6a868d000944b388335a01692722a453b0756
Will Farrington <wcfarrington@gmail.com>
2008-05-22 21:39:05 -0400

    Clean up the enabling and disabling.

--- rcirc-notify.el
+++ rcirc-notify.el
@@ -21,7 +21,8 @@
 
 (defvar rcirc-notification-message "%s is calling your name in %s."
   "Format of message to display in libnotify popup.
-'%s' will expand to the nick that notified you.")
+The first instance of '%s' will expand to the nick that notified you,
+while the latter will expand to the channel name or `rcirc-privmsg-target'.")
 
 (defvar rcirc-notified-nick-alist nil
   "An alist of nicks and the last time they tried to trigger a
@@ -32,7 +33,8 @@ notification.")
 same person.")
 
 (defvar rcirc-privmsg-target "private message"
-  "String used as target in rcirc-notify-message for private messages.")
+  "String used as target in `rcirc-notification-message' to denote
+private messages.")
 
 (defun rcirc-send-notification (title message)
   (cond ((eq window-system 'mac)
@@ -51,13 +53,9 @@ same person.")
     (let ((default-directory "~/"))
       (rcirc-send-notification "rcirc" (format rcirc-notification-message sender target))))
   ;; Always print a copy to *Messages*
-  (message (concat (format-time-string "%r") " - " (format rcirc-notification-message sender target))))
+  (message (string (format-time-string "%r") " - " (format rcirc-notification-message sender target))))
 
 (defun rcirc-notify-allowed (sender &optional delay)
-  "Return non-nil if a notification should be made for SENDER.
-If DELAY is specified, it will be the minimum time in seconds
-that can occur between two notifications.  The default is
-`rcirc-notification-timeout'."
   (unless delay (setq delay rcirc-notification-timeout))
   (let ((cur-time (time-to-seconds (current-time)))
         (cur-assoc (assoc sender rcirc-notified-nick-alist))
@@ -71,8 +69,6 @@ that can occur between two notifications.  The default is
       t)))
 
 (defun rcirc-notify-channel (proc sender response target text)
-  "Notify the current user when someone sends a message that
-matches a regexp in `rcirc-keywords' inside a channel."
   (interactive)
   (when (and (string-match
               (concat "\\(^\\|[^a-zA-Z0-9_\\-]\\)"
@@ -85,8 +81,6 @@ matches a regexp in `rcirc-keywords' inside a channel."
     (rcirc-notify sender target)))
 
 (defun rcirc-notify-privmsg (proc sender response target text)
-  "Notify the current user when someone sends a private message
-to them."
   (interactive)
   (when (and (string= response "PRIVMSG")
              (not (string= sender (rcirc-nick proc)))
@@ -96,16 +90,23 @@ to them."
 
 ;;;###autoload
 (defun turn-on-rcirc-notify ()
+  "Enables rcirc-notify if it hasn't already been loaded
+or required."
   (interactive)
-  (unless (load-history-filename-element "\\rcirc-notify.el")
+  (unless (featurep 'rcirc-notify)
     (load "rcirc-notify"))
   (add-hook 'rcirc-print-hooks 'rcirc-notify-privmsg)
   (add-hook 'rcirc-print-hooks 'rcirc-notify-channel))
 
 ;;;###autoload
 (defun turn-off-rcirc-notify ()
+  "Removes the rcirc-notify listening methods from
+`rcirc-print-hooks'."
   (interactive)
   (remove-hook 'rcirc-print-hooks 'rcirc-notify-channel)
   (remove-hook 'rcirc-print-hooks 'rcirc-notify-privmsg))
+
+(add-hook 'rcirc-print-hooks 'rcirc-notify-privmsg)
+(add-hook 'rcirc-print-hooks 'rcirc-notify-channel)
 
 (provide 'rcirc-notify)
